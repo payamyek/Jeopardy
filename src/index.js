98653bf18a2fd1608b28d8a4f2dfd97f50c845f4
payamyek <payamyek@gmail.com>
2020-04-23 19:00:04 -0400

    Cleaned up code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { render } from 'react-snapshot';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App/App';
 import * as serviceWorker from './serviceWorker';
 
-render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
