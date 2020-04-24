9b09225fcd426ff0e6838ede6554bcc49b6b428d
payamyek <payamyek@gmail.com>
2020-04-24 18:56:49 -0400

    Restructured Code

---
+++ src/Resources/sampleGameData.js
@@ -0,0 +1,139 @@
+const data = {
+    "Co-Workers": [
+        {
+            "Points": 100,
+            "Hint": "This person claims to have gone to over 500 concerts",
+            "Question": "Who is Octavian?"
+        },
+        {
+            "Points": 200,
+            "Hint": "This person's keyboard sounds like a jackhammer drilling in your ear",
+            "Question": "Who is Kevin?"
+        },
+        {
+            "Points": 300,
+            "Hint": "This person faked an one storey fall on camera for views",
+            "Question": "Who is Jason?"
+        },
+        {
+            "Points": 400,
+            "Hint": "This person was deserted by his loyal companion and was left to starve",
+            "Question": "Who is Carl?"
+        },
+        {
+            "Points": 500,
+            "Hint": "This young lad doesn't believe in drinking coffee",
+            "Question": "Who is Anand?"
+        }
+    ],
+    "Math" : [
+        {
+            "Points": 100,
+            "Hint": "The square root of 225",
+            "Question": "What is 15?"
+        },
+        {
+            "Points": 200,
+            "Hint": "This number has a day dedicated to it",
+            "Question": "What is pi?"
+        },
+        {
+            "Points": 300,
+            "Hint": "On a flat surface it's the shortest distance between two points",
+            "Question": "What is a straight line?"
+        },
+        {
+            "Points": 400,
+            "Hint": "The word is from the Arabic language meaning void even though the Mayans discovered the concept first",
+            "Question": "What is 0?"
+        },
+        {
+            "Points": 500,
+            "Hint": "This theorem states that every symmetric matrix is diagonalizable",
+            "Question": "What is the spectral theorem?"
+        }
+    ],
+    "Science" : [
+        {
+            "Points": 100,
+            "Hint": "This planet is the closest to the sun",
+            "Question": "What is Mercury?"
+        },
+        {
+            "Points": 200,
+            "Hint": "This metal is liquid at room temperature",
+            "Question": "What is Mercury?"
+        },
+        {
+            "Points": 300,
+            "Hint": "This person created the laws of motion",
+            "Question": "Who is Newton?"
+        },
+        {
+            "Points": 400,
+            "Hint": "This number represents the number of bones in the human body",
+            "Question": "What is 206?"
+        },
+        {
+            "Points": 500,
+            "Hint": "This cycle regulates the sleep-wake cycle and repeats roughly every 24 hours",
+            "Question": "What is the circadian rhythm?"
+        }
+    ],
+    "Movie Quotes" : [
+        {
+            "Points": 100,
+            "Hint": "\"To infinity and beyond!\"",
+            "Question": "What is toy story?"
+        },
+        {
+            "Points": 200,
+            "Hint": "\"I'll be back\"",
+            "Question": "What is the terminator?"
+        },
+        {
+            "Points": 300,
+            "Hint": "\"I am your father\"",
+            "Question": "What is starwars?"
+        },
+        {
+            "Points": 400,
+            "Hint": "\"Just keep swimming\"",
+            "Question": "What is finding nemo?"
+        },
+        {
+            "Points": 500,
+            "Hint": "\"I'm the king of the world\"",
+            "Question": "What is the titanic?"
+        }
+    ],
+    "Countries" : [
+        {
+            "Points": 100,
+            "Hint": "\"Eh, pass me the syrup!\"",
+            "Question": "What is canada?"
+        },
+        {
+            "Points": 200,
+            "Hint": "\"Why is the hospital so expensive, Bob?\"",
+            "Question": "What is america?"
+        },
+        {
+            "Points": 300,
+            "Hint": "\"Yo Jim, why is that kangaroo chasing us?\"",
+            "Question": "What is australia?"
+        },
+        {
+            "Points": 400,
+            "Hint": "\"Why is the pizza tilted?\"",
+            "Question": "What is italy?"
+        },
+        {
+            "Points": 500,
+            "Hint": "\"Why is their national animal an unicorn?\"",
+            "Question": "What is scotland?"
+        }
+    ]
+}
+
+export default data;
---
+++ src/Resources/snackbarResponse.js
@@ -0,0 +1,22 @@
+const correctResponses = [
+    "Wow, how did you get that one?",
+    "Are you cheating?",
+    "There is no way, are you a robot?",
+    "That one was way too easy, no need to celebrate.",
+    "I might have under-estimated you young jedi"
+]
+
+const incorrectResponses = [
+    "It isn't the first time you have failed us soldier",
+    "That one was easy, come on now!",
+    "Are you sure, you didn't know it?",
+    "Luke I am not your father",
+    "Someone disconnect their internet"
+]
+
+const snackbarResponse = {
+    "correct": correctResponses,
+    "incorrect": incorrectResponses
+}
+
+export default snackbarResponse;
