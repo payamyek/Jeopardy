4603e9d3609dae6ef11c8ef619ca55399a85a569
payamyek <payamyek@gmail.com>
2020-04-24 19:05:22 -0400

    Restructured Code

--- src/ActionCreators/UpdateTeamAMove.js
+++
@@ -1,3 +0,0 @@
-export default function updateTeamAMove(move) {
-    return {type: "UPDATE_TEAM_A_MOVE", payload: move}
-}
--- src/Resources/snackbarResponse.js
+++ src/Resources/snackbarResponse.js
@@ -3,7 +3,12 @@ const correctResponses = [
     "Are you cheating?",
     "There is no way, are you a robot?",
     "That one was way too easy, no need to celebrate.",
-    "I might have under-estimated you young jedi"
+    "I might have under-estimated you young jedi",
+    "Relax, it's just a game",
+    "How are you so calm?",
+    "Hey, stop cheating?",
+    "Big brother is watching!",
+    "Your parents must be proud!"
 ]
 
 const incorrectResponses = [
@@ -11,7 +16,11 @@ const incorrectResponses = [
     "That one was easy, come on now!",
     "Are you sure, you didn't know it?",
     "Luke I am not your father",
-    "Someone disconnect their internet"
+    "Someone disconnect their internet",
+    "What are you doing?",
+    "Booooooo!",
+    "How did you cheat and still get it wrong?",
+    "You have embarrassed your family"
 ]
 
 const snackbarResponse = {
