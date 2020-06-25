87241559d60bcf2baa2037d7dcd6616853c95050
payamyek <payamyektamaram@MacBook-Pro.local>
2020-06-24 22:46:44 -0400

    Proper Styling of Toast

--- src/Components/ToastNotification.js
+++ src/Components/ToastNotification.js
@@ -1,12 +1,25 @@
-import React from "react"
+import React from "react";
 import {toast} from "react-toastify";
+import {mdiEmoticonExcitedOutline, mdiEmoticonDeadOutline} from "@mdi/js";
+import Icon from "@mdi/react";
+
+
+function Body({message, icon}) {
+    return (
+        <table>
+            <tr>
+                <td><Icon path={icon} size={0.9}/></td>
+                <td>{message}</td>
+            </tr>
+        </table>
+    )
+}
 
 export default function displayNotification(message, type) {
     if (type === true) {
-        toast.success(`😀 ${message}`)
-    }
-    else {
-        toast.error(`😑 ${message}`)
+        toast.success(<Body message={message} icon={mdiEmoticonExcitedOutline}/>)
+    } else {
+        toast.error(<Body message={message} icon={mdiEmoticonDeadOutline}/>)
     }
 }
 
